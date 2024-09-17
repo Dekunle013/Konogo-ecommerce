98b88d4517dc7f50d8ec362b612e45f161e69461
Dekunle013 <Dekunle013@gmail.com>
2024-09-17 14:06:06 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Container from "./components/Container"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+
+function App() {
+
+  return (
+    <main>
+      <Header />
+      <Container>
+        <p>Ecommerce</p>
+      </Container>
+      <Footer />
+    </main>
+  )
+}
+
+export default App
