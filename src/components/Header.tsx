530e26d03a64137f2f6fa2b27e7889e27b404d0f
Dekunle013 <Dekunle013@gmail.com>
2024-09-18 05:42:24 +0100

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { logo } from '../assets';
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header className='w-full bg-whiteText'>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+    </header>
   )
 }
 
