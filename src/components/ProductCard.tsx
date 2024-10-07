292283b631998aab5c12a8cac5fdf52222eb5cd2
Dekunle013 <Dekunle013@gmail.com>
2024-10-07 20:50:09 +0100

    Updated the ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,105 +1,117 @@
-import React, { useState } from 'react';
-import { MdOutlineStarOutline } from 'react-icons/md';
-import { Button, Dialog, Transition } from "@headlessui/react";
+import { MdOutlineStarOutline } from "react-icons/md";
+import { ProductProps } from "../../type";
 import AddToCartButton from "./AddToCartButton";
+import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import FormattedPrice from "./FormattedPrice";
 import ProductCardSideNav from "./ProductCardSideNav";
 import { useNavigate } from "react-router-dom";
-
-interface ProductProps {
-  name: string;
-  overView: string;
-  regularPrice: number;
-  discountedPrice: number;
-  images: string[];
-}
-
 interface Props {
   item: ProductProps;
+  setSearchText?: any;
 }
 
-const ProductCard: React.FC<Props> = ({ item }) => {
+const ProductCard = ({ item, setSearchText }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
   const navigation = useNavigate();
 
-  const discountPercentage = ((item.regularPrice - item.discountedPrice) / item.regularPrice) * 100;
-  const handleProduct = () => {
-    navigation(`/product/${item?._id}`)
-  }
+  const open = () => {
+    setIsOpen(true);
+  };
+  const close = () => {
+    setIsOpen(false);
+  };
+  const percentage =
+    ((item?.regularPrice - item?.discountedPrice) / item?.regularPrice) * 100;
 
+  const handleProduct = () => {
+    navigation(`/product/${item?._id}`);
+    setSearchText && setSearchText("");
+  };
   return (
     <div className="border border-gray-200 rounded-lg p-1 overflow-hidden hover:border-black duration-200 cursor-pointer">
       <div className="w-full h-60 relative p-2 group">
-        <span onClick={openModal} className="bg-black text-skyText absolute left-0 top-0 w-16 text-xs text-center py-1 rounded-md font-semibold inline-block z-10">
-          save {discountPercentage.toFixed(0)}%
+        <span
+          onClick={open}
+          className="bg-black text-skyText absolute left-0 right-0 w-16 text-xs text-center py-1 rounded-md font-semibold inline-block z-10"
+        >
+          save {percentage.toFixed(0)}%
         </span>
-        <img 
+        <img
           onClick={handleProduct}
-          src={item.images[0]} 
-          alt={item.name} 
-          className="w-full h-full rounded-md object-cover group-hover:scale-110 duration-300"/>
-        <ProductCardSideNav />
+          src={item?.images[0]}
+          alt="productImage"
+          className="w-full h-full rounded-md object-cover group-hover:scale-110 duration-300"
+        />
+        <ProductCardSideNav product={item} />
       </div>
       <div className="flex flex-col gap-2 px-2 pb-2">
         <h3 className="text-xs uppercase font-semibold text-lightText">
-          {item.overView}
+          {item?.overView}
         </h3>
-        <h2 className="text-lg font-bold line-clamp-2">
-          {item.name}
-        </h2>
-        <div className="flex text-base text-lightText items-center">
-          {[...Array(5)].map((_, index) => (
-            <MdOutlineStarOutline key={index} />
-          ))}
+        <h2 className="text-lg font-bold line-clamp-2">{item?.name}</h2>
+        <div className="text-base text-lightText flex items-center">
+          <MdOutlineStarOutline />
+          <MdOutlineStarOutline />
+          <MdOutlineStarOutline />
+          <MdOutlineStarOutline />
+          <MdOutlineStarOutline />
         </div>
-        <AddToCartButton />
+        <AddToCartButton product={item} />
       </div>
-
-      <Transition show={isOpen} as={React.Fragment}>
-        <Dialog onClose={closeModal} className="relative z-10">
-          <Transition.Child
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
+      <Transition appear show={isOpen}>
+        <Dialog
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={close}
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <TransitionChild
                 enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
+                enterFrom="opacity-0 transform-[scale(95%)]"
+                enterTo="opacity-100 transform-[scale(100%)]"
                 leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
+                leaveFrom="opacity-100 transform-[scale(100%)]"
+                leaveTo="opacity-0 transform-[scale(95%)]"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                    Hurry Up!
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      This is a limited time offer for {item.name}. Don't miss out!
-                    </p>
-                  </div>
-
+                <DialogPanel className="w-full max-w-md rounded-xl bg-black backdrop-blur-2xl z-50 p-6">
+                  <DialogTitle
+                    as="h3"
+                    className="text-base/7 font-medium text-whiteText"
+                  >
+                    Hurry up!
+                  </DialogTitle>
+                  <p className="mt-2 text-sm/6 text-white/50">
+                    You are going to save{" "}
+                    <span className="text-skyText">
+                      <FormattedPrice
+                        amount={item?.regularPrice - item?.discountedPrice}
+                      />{" "}
+                    </span>
+                    from this product.
+                  </p>
+                  <p className="text-sm/6 text-white/50">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sequi, consequatur?
+                  </p>
                   <div className="mt-4">
                     <Button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      onClick={close}
                     >
                       Got it, thanks!
                     </Button>
                   </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                </DialogPanel>
+              </TransitionChild>
             </div>
           </div>
         </Dialog>
